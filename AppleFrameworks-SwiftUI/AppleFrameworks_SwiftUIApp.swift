d84bb208caa7d8019b637b028ecaf003a7bba8e8
Manuel Alejandro <alejandro>
2023-08-28 17:01:21 -0700

    Initial Commit

---
+++ AppleFrameworks-SwiftUI/AppleFrameworks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworks_SwiftUIApp.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Administrador on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
